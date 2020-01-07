a45fb210f26f519fa511ced05ca52b091c204d21
Lehtinen Teemu <lehtint6@t31300-lr146.org.aalto.fi>
2020-01-07 09:22:31 +0200

    Upgrade tests to current wdio and chai

--- tests/test.js
+++ tests/test.js
@@ -1,85 +1,106 @@
-describe('acos-jsparsons-custom', function() {
+let chai = require('chai');
+chai.should();
 
-	describe('URL exercises', function() {
-  	it('should load and solve an exercise from GET parameters', function() {
-  		return browser
-  			.url('/html/jsparsons/jsparsons-python-custom/exercise?name=Test&initial=if%20True%3A%5Cn%20%20print(%5C%22Test%20succeeded%5C%22)&instructions=Instructions%20for%20test%20exercise&description=Testing%20exercise')
-  			.isVisible('#ul-sortableTrash').then(function(vis) {
-  				vis.should.be.equal(true);
-  			})
-        .isVisible('#sortable').then(function(vis) {
-          vis.should.be.equal(true);
-        })
-        .moveToObject('#sortablecodeline0', 15,15)
-        .buttonDown()
-        .moveToObject('#ul-sortable', 35, 5)
-        .buttonUp()
-        .click('#feedbackLink')
-        .alertAccept().then(function(a) {
-          a.state.should.be.equal('success');
-        })
-        .getAttribute('#ul-sortable', 'class').then(function(classes) {
-          classes.should.contain('incorrect');
-        })
-        .moveToObject('#sortablecodeline1', 15,15)
-        .buttonDown()
-        .moveToObject('#ul-sortable', 75, 25)
-        .buttonUp()
-        .click('#feedbackLink')
-        .getAttribute('#ul-sortable', 'class').then(function(classes) {
-          classes.should.contain('correct');
-          classes.should.not.contain('incorrect');
-        });
-  	});
-	});
+describe('Content module acos-jsparsons-custom', () => {
 
-  describe('JSON exercises', function() {
-    var http = require('http');
-    var fs = require('fs');
-    var jsonContent = JSON.stringify({
-      "name": "Name",
-      "description": "Example Description",
-      "instructions": "Example instructions",
-      "initial": "print(\"Hello\")\n  print(\"World2)"
+  function hasEachOnce(selectors) {
+    selectors.forEach(selector => {
+      let matches = $$(selector);
+      matches.should.have.lengthOf(1);
+      matches[0].isDisplayed().should.be.true;
     });
-    function handleRequest(request, response){ //always return example.json
+  }
+
+  function dragAndDropNear(source, target, x, y) {
+    var loc = source.getLocation();
+    let sourceX = parseInt(loc.x + 5);
+    let sourceY = parseInt(loc.y + 5);
+    loc = target.getLocation();
+    let targetX = parseInt(loc.x + x) - sourceX;
+    let targetY = parseInt(loc.y + y) - sourceY;
+    browser.performActions([{
+      type: 'pointer',
+      id: 'finger1',
+      parameters: { pointerType: 'mouse' },
+      actions: [
+        { type: 'pointerMove', duration: 0, x: sourceX, y: sourceY },
+        { type: 'pointerDown', button: 0 },
+        { type: 'pause', duration: 10 },
+        { type: 'pointerMove', duration: 100, origin: 'pointer', x: targetX, y: targetY },
+        { type: 'pointerUp', button: 0 }
+      ]
+    }]);
+  }
+
+  describe('URL exercises', () => {
+
+    it('should load an exercise from GET parameters', () => {
+      browser.url('/html/jsparsons/jsparsons-python-custom/exercise?name=Test&initial=if%20True%3A%5Cn%20%20print(%5C%22Test%20succeeded%5C%22)&instructions=Instructions%20for%20test%20exercise&description=Testing%20exercise');
+      hasEachOnce(['#ul-sortableTrash', '#sortable']);
+    });
+
+    it('first line added to solution should yield incorrect', () => {
+      dragAndDropNear($('#sortablecodeline0'), $('#ul-sortable'), 10, 10);
+      $('#feedbackLink').click();
+      browser.acceptAlert();
+      $('#ul-sortable').getAttribute('class').should.contain('incorrect');
+    });
+
+    it('second line added to solution should yield correct', () => {
+      dragAndDropNear($('#sortablecodeline1'), $('#ul-sortable'), 60, 40);
+      $('#feedbackLink').click();
+      let cls = $('#ul-sortable').getAttribute('class');
+      cls.should.not.contain('incorrect');
+      cls.should.contain('correct');
+    });
+
+  });
+
+  describe('JSON exercises', () => {
+    var server = null;
+
+    it('should start test service to provide json', () => {
+      let http = require('http');
+      //var fs = require('fs');
+      let jsonContent = JSON.stringify({
+        "name": "Name",
+        "description": "Example Description",
+        "instructions": "Example instructions",
+        "initial": "print(\"Hello\")\nprint(\"World\")"
+      });
+      function handleRequest(request, response){ //always return example.json
+          response.end(jsonContent);
+      }
+      server = http.createServer((request, response) => {
         response.end(jsonContent);
-    }
-    var server = http.createServer(handleRequest);
-    server.listen(8011, function(){ });
+      });
+      server.listen(8011, () => {});
+    });
 
-    it('should load an exercise from an external .json file ', function() {
-      return browser
-        .url('/html/jsparsons/jsparsons-python-custom/exercise?url=http://localhost:8011/example.json')
-        .isVisible('#ul-sortableTrash').then(function(vis) {
-          vis.should.be.equal(true);
-        })
-        .isVisible('#sortable').then(function(vis) {
-          vis.should.be.equal(true);
-        })
-        .moveToObject('#sortablecodeline0', 15,15)
-        .buttonDown()
-        .moveToObject('#ul-sortable', 35, 5)
-        .buttonUp()
-        .click('#feedbackLink')
-        .alertAccept().then(function(a) {
-          a.state.should.be.equal('success');
-        })
-        .getAttribute('#ul-sortable', 'class').then(function(classes) {
-          classes.should.contain('incorrect');
-        })
-        .moveToObject('#sortablecodeline1', 15,15)
-        .buttonDown()
-        .moveToObject('#ul-sortable', 75, 25)
-        .buttonUp()
-        .click('#feedbackLink')
-        .getAttribute('#ul-sortable', 'class').then(function(classes) {
-          classes.should.contain('correct');
-          classes.should.not.contain('incorrect');
-        }).then(function() {
-          server.close();
-        });
-    });      
+    it('should load an exercise from JSON resource', () => {
+      browser.url('/html/jsparsons/jsparsons-python-custom/exercise?url=http://localhost:8011/example.json');
+      hasEachOnce(['#ul-sortableTrash', '#sortable']);
+    });
 
-  });  
+    it('first line added to solution should yield incorrect', () => {
+      dragAndDropNear($('#sortablecodeline0'), $('#ul-sortable'), 10, 10);
+      $('#feedbackLink').click();
+      browser.acceptAlert();
+      $('#ul-sortable').getAttribute('class').should.contain('incorrect');
+    });
+
+    it('second line added to solution should yield correct', () => {
+      dragAndDropNear($('#sortablecodeline1'), $('#ul-sortable'), 10, 40);
+      $('#feedbackLink').click();
+      let cls = $('#ul-sortable').getAttribute('class');
+      cls.should.not.contain('incorrect');
+      cls.should.contain('correct');
+    });
+
+    it('should stop test service', () => {
+      server.close();
+    });
+
+  });
+
 });
